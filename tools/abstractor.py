325d9201a3cd8e102e343f92eb1057d07acbdd56
Abbas <fadox@users.noreply.github.com>
2016-12-22 11:02:53 +0100

    add glyphs to background layers

--- tools/abstractor.py
+++ tools/abstractor.py
@@ -1,4 +1,4 @@
-from defcon import Font
+from defcon import Font, Color
 from argl import *
 mFont = Font("../sources/lots.ufo")
 mUnicodes = mFont.unicodeData
@@ -6,13 +6,17 @@ data = mFont.getDataForSerialization()
 
 ufo = Font()
 ufo.setDataFromSerialization({'info': data['info']})
-ufo.layers.newLayer('L1')
+layersDict = {}
+for nl in range(1,9):
+    newLayer = ufo.layers.newLayer('background'+str(nl))
+    newLayer.color = Color('1,0,0,1')
+    layersDict.update({'layer'+str(nl):newLayer})
 
 for glf in RAWN2C:
     layer = 0
     glfSrc = RAWN2C[glf].split(',')
     for g in glfSrc:
-        if layer == 2:
+        if layer == 10:
             continue
         try:
             gCode = AGL2UV[g]
@@ -21,13 +25,15 @@ for glf in RAWN2C:
             glyph.name = glf
             glyph.unicode = 0
             if layer > 0:
-                glyph.lib._set_layer('L1')
-            ufo.insertGlyph(glyph)
+                currentLayer = layersDict['layer'+str(layer)]
+                currentLayer.insertGlyph(glyph)
+            else:
+                ufo.insertGlyph(glyph)
             layer += 1
             print(g + ' found :)' + '  L ' + str(layer))
         except:
             print('         '+g+' not found in font')
 for l in ufo.layers:
-    print l.name
-#print(ufo.layers)
+    print (l.name)
+#print(ufo.glyphSet)
 ufo.save('xxx.ufo')
